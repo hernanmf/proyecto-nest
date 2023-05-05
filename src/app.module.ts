6dca8520e7bf52ad8bb584b4a1726599f37a5460
hernanmf <hernanmartinezf@gmail.com>
2023-05-04 21:28:18 -0300

    instaladas importaciones para la parte estatica

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AutosModule } from "./autos/autos.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from 'path';
 
 @Module({
-  imports: [AutosModule],
+  imports: [AutosModule,ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'client') })],
   controllers: [],
   providers: [],
 })
