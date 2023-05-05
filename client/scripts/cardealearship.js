a830e8f9e52ea6598dc5e510d42b07cde0355a2a
Hernan Martinez <hernanmartinezf@gmail.com>
2023-05-05 19:08:58 -0300

    funciona el get, intentando el getbyid

--- client/scripts/cardealearship.js
+++ client/scripts/cardealearship.js
@@ -1,10 +1,8 @@
 const url_base = "http://localhost:3000";
 const endpoint = "/autos";
-const d = document;
 let autos = [];
 
-let contenedor = d.getElementById("tblAutos");
-let btnVerAuto = d.getElementById("btnVerAuto");
+let contenedor = document.getElementById("tblAutos");
 
 const mostrarAutos = () => {
   let tabla = "";
@@ -23,6 +21,20 @@ const mostrarAutos = () => {
          /* <td> <button type="button" class="btnEliminar" id="${r.id}">Eliminar</button></td>  */
 
   contenedor.innerHTML = tabla;
+  console.log("se hizo tabla");
+  const btnVerAutos = document.querySelectorAll('.btnVerAuto');
+  let btnVerAutosListeners = [];
+ console.log("se genero array");
+  btnVerAutos.forEach((btnVerAuto) => {
+    const autoId = btnVerAutos.value;
+    const btnVerAutoListener = () => {
+      alert(`Has hecho clic en el botón Ver Auto con el ID ${autoId}.`);
+    // Aquí puedes hacer lo que necesites con el ID del auto
+    };
+    btnVerAutosListeners.push(btnVerAutoListener);
+  });
+  console.log("se generaron los eventlister");
+  
 }
 
   async function loadAutos() {
@@ -35,20 +47,20 @@ const mostrarAutos = () => {
 
 loadAutos();
 
-btnVerAuto.addEventListener('click', () => {
-  const idAuto = btnVerAuto.value;
-  verAuto(idAuto);
-});
+//usamos el evento DOMCONTENTLOADED para que se genere el eventlistener del boton una vez generado sino da null
 
-function verAuto(idAuto) {
+
+
+
+/* function verAuto(idAuto) {
   fetch(url_base + endpoint +"/"+idAuto)
     .then(response => response.json())
     .then(auto => {
       console.log(auto);
-      alert(`El resultado del endpoint es: ${auto}`);
+      alert(`El resultado del endpoint es: ${idAuto}`);
     })
     .catch(error => {
       alert('Ha ocurrido un error al obtener el auto que querias ver.');
       console.error(error);
     });
-}
+} */
